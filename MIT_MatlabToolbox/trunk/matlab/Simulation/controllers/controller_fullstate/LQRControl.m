25cdae1f5869176212b796e505c3494bd18056f7
Samuel Udotong <sudotong@mit.edu>
2015-11-12 10:32:54 -0500

    adding the files for lab3 part 1 i think

--- MIT_MatlabToolbox/trunk/matlab/Simulation/controllers/controller_fullstate/LQRControl.m
+++ MIT_MatlabToolbox/trunk/matlab/Simulation/controllers/controller_fullstate/LQRControl.m
@@ -28,15 +28,15 @@ dpos_max    = 1;
 datt_max    = 1;
 
 %Limit on control input
-motor_max = 1200;          % 500
+motor_max = 500;          % 1200
 
 %Cost weights on states
-pos_x_wght = 0.15/3;       % 0.25/3
-pos_y_wght = 0.15/3;       % 0.25/3
-pos_z_wght = 0.95/3;       % 0.25/3
-att_wght   = 0.075/3;      % 0.175/3
-dpos_wght  = 0.075/3;      % 0.175/3
-datt_wght  = 0.3  /3;      % 0.4/3
+pos_x_wght = 0.25/3;       % 0.15/3
+pos_y_wght = 0.25/3;       % 0.15/3
+pos_z_wght = 0.25/3;       % 0.95/3
+att_wght   = 0.175/3;      % 0.075/3
+dpos_wght  = 0.175/3;      % 0.075/3
+datt_wght  = 0.4  /3;      % 0.3/3
 
 rho = 0.05;
 
